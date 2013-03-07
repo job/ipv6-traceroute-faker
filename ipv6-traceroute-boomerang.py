96bab6e645ff5b6bce6cb3e1793447a3979b64a1
Job Snijders <job@instituut.net>
2013-03-07 23:22:00 +0100

    added example output

--- ipv6-traceroute-boomerang.py
+++ ipv6-traceroute-boomerang.py
@@ -19,7 +19,45 @@
 # the midpoint will be spoofed in such a way that effectively the originator
 # ends up with a traceroute back to him or herself. So in one traceroute
 # you can discover both the forward path, and the path _back_.
-
+# 
+# Example output which illustrates the path from iij01.ring.nlnog.net (in Japan)
+# to boomerang.atrato.net, and back to iij01. Starting from hop 16 the packets
+# are mangled by this python script. 
+#
+#   job@iij01.ring.nlnog.net:~$ mtr -r -c 1 -w -6 boomerang.atrato.net
+#   HOST: iij01.ring.nlnog.net              Loss%   Snt   Last   Avg  Best  Wrst StDev
+#     1.|-- 2001:240:10c:5:219:30ff:fec6:d49b  0.0%     2    0.5   0.5   0.5   0.6   0.1
+#     2.|-- 2001:240:bb43:1002::2              0.0%     1    1.3   1.3   1.3   1.3   0.0
+#     3.|-- tky006bb01.IIJ.Net                 0.0%     1    1.0   1.0   1.0   1.0   0.0
+#     4.|-- 2001:240:bb00:8021::7e             0.0%     1    1.2   1.2   1.2   1.2   0.0
+#     5.|-- tky001bf01.IIJ.Net                 0.0%     1    1.1   1.1   1.1   1.1   0.0
+#     6.|-- sjc002bf00.iij.net                 0.0%     1  106.0 106.0 106.0 106.0   0.0
+#     7.|-- plt001bb00.iij.net                 0.0%     1  106.9 106.9 106.9 106.9   0.0
+#     8.|-- plt001bb01.iij.net                 0.0%     1  106.8 106.8 106.8 106.8   0.0
+#     9.|-- tge1-3.sjo01-1.us.as5580.net       0.0%     1  119.8 119.8 119.8 119.8   0.0
+#    10.|-- eth4-2.r1.den1.us.atrato.net       0.0%     1  130.5 130.5 130.5 130.5   0.0
+#    11.|-- eth3-1.r1.chi1.us.atrato.net       0.0%     1  176.1 176.1 176.1 176.1   0.0
+#    12.|-- eth1-4.r1.nyc1.us.atrato.net       0.0%     1  198.9 198.9 198.9 198.9   0.0
+#    13.|-- eth1-5.core1.lon1.uk.atrato.net    0.0%     1  257.9 257.9 257.9 257.9   0.0
+#    14.|-- 2a02:d28:5580:1::96                0.0%     1  264.0 264.0 264.0 264.0   0.0
+#    15.|-- eth4-1.r1.ams2.nl.atrato.net       0.0%     1  276.5 276.5 276.5 276.5   0.0
+#    16.|-- boomerang.atrato.net               0.0%     1  270.0 270.0 270.0 270.0   0.0
+#    17.|-- ve1007.r1.ams2.nl.atrato.net       0.0%     1  281.5 281.5 281.5 281.5   0.0
+#    18.|-- eth1-1.core1.ams2.nl.atrato.net    0.0%     1  267.3 267.3 267.3 267.3   0.0
+#    19.|-- 2a02:d28:5580:1::95                0.0%     1  266.9 266.9 266.9 266.9   0.0
+#    20.|-- eth4-2.r1.nyc1.us.atrato.net       0.0%     1  301.2 301.2 301.2 301.2   0.0
+#    21.|-- ipv6-iij.net                       0.0%     1  281.1 281.1 281.1 281.1   0.0
+#    22.|-- nyc002bb11.iij.net                 0.0%     1  271.9 271.9 271.9 271.9   0.0
+#    23.|-- sjc002bb01.iij.net                 0.0%     1  258.3 258.3 258.3 258.3   0.0
+#    24.|-- sjc002bf02.iij.net                 0.0%     1  262.4 262.4 262.4 262.4   0.0
+#    25.|-- 2001:48b0:bb00:803e::75            0.0%     1  261.2 261.2 261.2 261.2   0.0
+#    26.|-- tky001bb11.IIJ.Net                 0.0%     1  261.2 261.2 261.2 261.2   0.0
+#    27.|-- tky006bb01.IIJ.Net                 0.0%     1  261.8 261.8 261.8 261.8   0.0
+#    28.|-- tky006gate01a.IIJ.Net              0.0%     1  261.3 261.3 261.3 261.3   0.0
+#    29.|-- 2001:240:bb43:1002::6              0.0%     1  261.1 261.1 261.1 261.1   0.0
+#    30.|-- ???                               100.0     1    0.0   0.0   0.0   0.0   0.0
+#   job@iij01.ring.nlnog.net:~$
+#   
 boomerang_target    = "2a02:d28:601:2::1"
 boomerang_midpoint  = "2a02:d28:601:2::"
 netfilterqueue      = 1
